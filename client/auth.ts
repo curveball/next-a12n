7d4851066180de6a118ff1860ec35e02d30ab79f
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-02-01 12:36:19 -0500

    build: comment out unrecognized json input so the project can build

--- client/auth.ts
+++ client/auth.ts
@@ -5,17 +5,17 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   debug: true,
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
-    {
-      id: "a12n-server", // signIn("my-provider") and will be part of the callback URL
-      name: "a12n-server", // optional, theoretically used on the default login page as the button text.
-      type: "oauth",
-      issuer: process.env.NEXTAUTH_INTERNAL_URL + `/.well-known/oauth-authorization-server`,
-      clientId: process.env.NEXTAUTH_CLIENT_ID, 
-      clientSecret: process.env.NEXTAUTH_SECRET, 
-      authorization: process.env.NEXTAUTH_INTERNAL_URL + `/authorize`,
-      token: process.env.NEXTAUTH_INTERNAL_URL + `/token`,
-      // userinfo: process.env.NEXTAUTH_INTERNAL_URL + `/user/:id`,
-    }
+    // {
+    //   id: "a12n-server", // signIn("my-provider") and will be part of the callback URL
+    //   name: "a12n-server", // optional, theoretically used on the default login page as the button text.
+    //   type: "oauth",
+    //   issuer: process.env.NEXTAUTH_INTERNAL_URL + `/.well-known/oauth-authorization-server`,
+    //   clientId: process.env.NEXTAUTH_CLIENT_ID, 
+    //   clientSecret: process.env.NEXTAUTH_SECRET, 
+    //   authorization: process.env.NEXTAUTH_INTERNAL_URL + `/authorize`,
+    //   token: process.env.NEXTAUTH_INTERNAL_URL + `/token`,
+    //   // userinfo: process.env.NEXTAUTH_INTERNAL_URL + `/user/:id`,
+    // }
   ],
     pages: {
       signIn: "/login", // map to a custom front end login url 
