e727a3f0762bc8a585a9e21b48d2712861b4c871
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-02-01 12:11:38 -0500

    refactor: should have need awareness of username and password

--- client/app/actions/login.tsx
+++ client/app/actions/login.tsx
@@ -1,33 +1,14 @@
 import { signIn } from "@/auth";
 import { AuthError } from "next-auth";
 import { redirect } from "next/navigation";
-import { z } from "zod";
 
-const loginSchema = z.object({
-    username: z.string().email({
-        message: "Invalid email address.",
-    }),
-    password: z.string().min(6, {
-        message: "Password must be at least 6 characters.",
-    }).trim()
-})
-
-export async function login(formData: FormData) {
+export async function login() {
     "use server"
-    const validatedFields = loginSchema.safeParse({
-        userName: formData.get('userName'),
-        password: formData.get('password')
-    })
-    if (!validatedFields.success) {
-        return { error: validatedFields.error.errors }
-    }
-
     try {
-        await signIn("a12n-server", formData)
+        await signIn("a12n-server")
     } catch (error) {
         if (error instanceof AuthError) {
             return redirect(`${process.env.NEXT_ERROR_URL}?error=${error.type}`)
         }
-        throw error
     }
 }
