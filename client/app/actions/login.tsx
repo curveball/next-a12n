0bc77066916870ae11e1a1bcf008df3fd039afe8
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-02-02 16:42:36 -0500

    redirect to signin url

--- client/app/actions/login.tsx
+++ client/app/actions/login.tsx
@@ -8,7 +8,7 @@ export async function login() {
         await signIn("a12n-server")
     } catch (error) {
         if (error instanceof AuthError) {
-            return redirect(`${process.env.NEXT_ERROR_URL}?error=${error.type}`)
+            return redirect(`${process.env.NEXTAUTH_ERROR_URL}`)
         }
     } finally {
         return redirect(`${process.env.NEXTAUTH_URL}/api/auth/signin/a12n-server`)
