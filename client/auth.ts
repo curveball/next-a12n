695c3abed31872f0a27a71e55b0294ebf1f14d80
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-02-02 16:40:46 -0500

    refactor: wellKnown prop no longer needed

--- client/auth.ts
+++ client/auth.ts
@@ -7,17 +7,17 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
     {
-      id: "a12n-server", // signIn("my-provider") and will be part of the callback URL
-      name: "a12n-server", // optional, theoretically used on the default login page as the button text.
+      id: "a12n-server",
+      name: "a12n-server",
       type: "oidc",
       issuer: process.env.NEXTAUTH_URL,
       clientId: process.env.NEXTAUTH_CLIENT_ID, 
       clientSecret: process.env.NEXTAUTH_SECRET, 
       authorization: process.env.NEXTAUTH_URL + "/authorize",
-      wellKnown: process.env.NEXTAUTH_URL + "/.well-known/openid-configuration",
     }
   ],
-    pages: {
-      signIn: "/login", // map to a custom front end login url 
+  pages: {
+    signIn: "/login", // map to a custom front end login url,
+    signOut: "/logout", // map to a custom front end logout url,
     },
   })
