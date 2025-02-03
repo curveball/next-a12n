e47ce905d5b63221854aeb7aefc3ab56a0927c06
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-02-03 09:27:10 -0500

    feat: adds error page and more logging

--- client/app/actions/login.tsx
+++ client/app/actions/login.tsx
@@ -1,16 +1,34 @@
 import { signIn } from "@/auth";
 import { AuthError } from "next-auth";
+import { getRedirectStatusCodeFromError, getRedirectTypeFromError } from "next/dist/client/components/redirect";
 import { redirect } from "next/navigation";
 
 export async function login() {
     "use server"
     try {
-        await signIn("a12n-server")
+        const authResult = await signIn("a12n-server")
+        console.info(`::::: authResult::::: `, authResult)
     } catch (error) {
-        if (error instanceof AuthError) {
-            return redirect(`${process.env.A12N_URL}/login?continue=`)
+
+        if ('digest' in error) {
+            const errorType = getRedirectTypeFromError(error);
+            const errorCode = getRedirectStatusCodeFromError(error);
+            redirect(`/error?error=${errorType}&code=${errorCode}`);
         }
-    } finally {
-        return redirect(`${process.env.A12N_URL}/api/auth/signin/a12n-server`)
+        if (error instanceof Error) {
+            const { type, cause } = error as AuthError;
+            switch (type) {
+                case "InvalidCallbackUrl":
+                    return "Invalid callback URL.";
+                case "CredentialsSignin":
+                    return "Invalid credentials.";
+                case "CallbackRouteError":
+                    return cause?.err?.toString();
+                default:
+                    redirect(`/error?error=${type}`);
+            }
+        }
+
+        throw error;
     }
 }
