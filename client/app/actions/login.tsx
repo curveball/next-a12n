c383051fb1aa13c2ef2fcc6a2822e9b64cc1f086
Jen Chan <jennifer@rivalry.com>
2025-02-06 12:50:46 -0500

    refactor: adds signIn callback with correct redirects post-auth

--- client/app/actions/login.tsx
+++ client/app/actions/login.tsx
@@ -1,6 +1,6 @@
 
 "use server"
-import { auth, signIn } from "@/auth";
+import { signIn } from "@/auth";
 import { AuthError } from "next-auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 import { redirect } from "next/navigation";
@@ -8,7 +8,7 @@ import { redirect } from "next/navigation";
 export async function login() {
 
     try {
-        await signIn("a12n-server");
+        await signIn("a12n-server")
     } catch (error) {
 
         if (isRedirectError(error)) {
@@ -19,12 +19,5 @@ export async function login() {
             return redirect(`${process.env.NEXTAUTH_URL}?error=${error.type}`)
         }
         throw error
-    } finally {
-        const session = await auth()
-        if (session?.user) {
-            // Login success, redirect to home
-            return redirect("/")
-        }
-        redirect("/login")
     }
 }
--- client/auth.ts
+++ client/auth.ts
@@ -5,21 +5,26 @@ import NextAuth, { Account, Profile } from "next-auth";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   debug: process.env.NODE_ENV !== "production",
   secret: process.env.AUTH_SECRET,
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+  
+      console.log(`signIn `, user, account, profile, email, credentials)
+      // if signin successful, redirect to home, otherwise redirect to login page
+      return user !== null ? `${process.env.NEXTAUTH_URL}` : `${process.env.AUTH_A12N_ISSUER}/login`
+    },
+  },
   providers: [
     {
       id: "a12n-server",
       name: "a12n-server",
       type: "oidc",
       issuer: process.env.AUTH_A12N_ISSUER,
-      clientId: process.env.AUTH_A12N_ID, 
+      clientId: process.env.AUTH_A12N_ID,
       clientSecret: process.env.AUTH_A12N_SECRET,
-    }
+    },
   ],
   session: { strategy: "jwt" },
-  pages: {
-    signIn: "/login"
-  }
-  })
+})
 
 declare module "next-auth" {
   interface Session {
