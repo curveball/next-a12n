c6358ad00f0a4f20331fea0a277a3facdb39e72d
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-02-09 11:12:00 -0500

    feat: remove A12N cookie on logout

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,1 +1,14 @@
-export { auth as middleware } from "./auth";
+import { cookies } from "next/headers";
+import { NextRequest } from "next/server";
+import { auth as middleware } from "./auth";
+
+export default async function auth(req: NextRequest) {
+    if (req.nextUrl.pathname.startsWith('/api/auth')) {
+        if (req.nextUrl.pathname.endsWith('/signout')) {
+            const cookieStore = await cookies()
+            cookieStore.delete('A12N')
+        }
+    }
+
+    return middleware(req);
+}
