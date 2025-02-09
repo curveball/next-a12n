c2e7591795a3ec82a973eac28ea5a3c5896df7f8
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-02-09 10:18:14 -0500

    style: put a12n-server text in <pre> tags

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -5,13 +5,12 @@ import SessionData from "./session-data";
 
 export default async function Index() {
     const session = await auth()
-    console.log(`::::: index page session::::: `, session)
     return (
         <div className="flex flex-col gap-6 px-4 py-8">
             <h1 className="text-3xl font-bold">Next.js a12n-server</h1>
             <div>
                 This is an example site to demonstrate how to use{" "}
-                <Link href="https://nextjs.authjs.dev">NextAuth.js</Link> with <Link href="">`a12n-server`</Link>
+                <Link href="https://nextjs.authjs.dev">NextAuth.js</Link> with <Link href=""><pre className="inline px-1">a12n-server</pre></Link>
                 for authentication.
             </div>
             <div className="flex flex-col rounded-md bg-gray-100">
